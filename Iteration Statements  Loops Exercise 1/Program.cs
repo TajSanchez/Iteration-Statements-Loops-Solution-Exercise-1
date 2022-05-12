ea11960d6e702da195dfbca53a49da94ba294314
Taj <tajmarie92@gmail.com>
2022-05-11 21:08:50 -0500

    Add project files.

---
+++ Iteration Statements  Loops Exercise 1/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace Iteration_Statements__Loops_Exercise_1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            for (int i = 0; i < 14; i++)
+            {
+
+            Console.WriteLine(i);
+            } 
+        }
+
+
+    }
+}
+    
